29e675e6dc132ee8ba4dc88aba3d44418f4bff09
Vincent Pfister <vincent.pfister@raisepartner.com>
2020-07-01 01:10:49 +0200

    add server logging

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import time
+import logging
 import uuid
 from concurrent import futures
 
@@ -8,10 +8,12 @@ import getid_pb2
 import getid_pb2_grpc
 
 id_ = uuid.uuid4()
+logger = logging.getLogger(__name__)
 
 
 class Information(getid_pb2_grpc.InformationServicer):
     def RequestID(self, request, context):
+        logger.info("RequestId")
         return getid_pb2.IDReply(message='ID: %s' % id_)
 
 
@@ -19,13 +21,12 @@ def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     getid_pb2_grpc.add_InformationServicer_to_server(Information(), server)
     server.add_insecure_port('[::]:50051')
+    logger.info("starting server")
     server.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        server.stop(0)
+    server.wait_for_termination()
+    logger.info("server stopped")
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     serve()
